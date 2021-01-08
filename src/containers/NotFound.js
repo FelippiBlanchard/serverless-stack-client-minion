bb4c2d1d0823042612ab2455b3ad03d8da343377
FelippiBlanchard <febeck1@hotmail.com>
2021-01-08 09:46:37 -0300

    Atualizacao NotFound

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./NotFound.css";
 import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
